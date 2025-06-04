5711d8b2f77dd0edd9e5131d7789e6e5df39aca9
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-06-04 12:21:53 -0400

    Update astro.config.mjs to include site and base settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // outDir: './docs',
+    site: 'https://liosvel-medina.github.io',
+    base: '/zait_and_ghaz_landing'
+});
