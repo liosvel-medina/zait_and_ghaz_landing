b00cdccffd4ab2cf2238f652658aa384ee612200
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-06-03 22:32:25 -0400

    Update base path in astro.config.mjs and add Poppins font styles in new CSS file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     outDir: './docs',
-    base: '/zait_and_ghaz_landing/'
+    base: '/docs/'
 });
